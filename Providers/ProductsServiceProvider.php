d68752dabab1b890a845e394c9f388cf8f9d9739
Rafał Purwin <purwinr@gmail.com>
2019-08-22 08:38:16 +0200

    tt

---
+++ Providers/ProductsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Plugins\Futurerafal\Plugin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
