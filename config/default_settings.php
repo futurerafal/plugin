e7ad42904218b6ac9930b5b0e176ddbba24e46f8
Rafał Purwin <purwinr@gmail.com>
2019-09-05 10:59:59 +0200

    conf

--- config/default_settings.php
+++ config/default_settings.php
@@ -10,13 +10,13 @@ return [
         'rules' => [
             'plugin' => [
                 'update' => [
-                    'foo_bar' => 'string',
+                    'general.foo_bar' => 'string',
                 ],
             ],
         ],
         'visibility' => [
             'plugin' => [
-                'foo_bar' => 'list:guest,customer,admin|details:guest,customer,admin',
+                'general.foo_bar' => 'list:guest,customer,admin|details:guest,customer,admin',
             ],
             'shipping' => [
                 'methods.braintree.merchantId' => 'list:guest,customer,admin|details:admin'
